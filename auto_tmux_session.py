c38deaf0dd889c32611bc1cb7709ca7f50915373
Loi Huu Tran <loith.wts@gmail.com>
2022-08-14 17:39:28 +0700

    Update: change algorithm logical operations

--- auto_tmux_session.py
+++ auto_tmux_session.py
@@ -1,20 +1,17 @@
+import json
 import os 
-import sys
-from configs import Configuration
 
-dataset = 'wiki'
-
-if len(sys.argv) > 1:
-    dataset = str(sys.argv[1])
-
-config = Configuration()
-
-tmux_session = (
-        lambda alg, ds, k: 
-        f"tmux new-session -d -s {alg}_{ds}_k{k} 'python3 run_params.py {alg} {ds} {k}'"
-        )
-
-for alg in config.get_alg_ids():
-    for k in config.k_values:
-        os.system(tmux_session(alg, dataset, k))
-
+with open('run.json', 'r') as file:
+    settings = json.load(file)["settings"]
+    tmux_session = (
+            lambda alg, ds, k, b: 
+            f"tmux new-session -d -s {alg}_{ds}_k{k}_b{b} 'python3 run_params.py {alg} {ds} {k} {b}'"
+            )
+    for setting in settings:
+        os.system(
+                tmux_session(
+                    setting['alg'], 
+                    setting['dataset'],
+                    setting['k'],
+                    setting['b']
+            ))
